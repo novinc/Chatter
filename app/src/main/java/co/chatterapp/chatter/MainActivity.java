b56f00797d2c652d2e032d517833430269b2e266
heyitsjeoff <heyitsjeoff@gmail.com>
2015-10-17 20:34:26 -0700

    jeoff comment

--- app/src/main/java/co/chatterapp/chatter/MainActivity.java
+++ app/src/main/java/co/chatterapp/chatter/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+//jeoff comment
 
 public class MainActivity extends AppCompatActivity {
 
